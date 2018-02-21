f14395090359a16243070ba048aa12fc938d93d2
AngelinaBalliro <35516348+aeoniandesign@users.noreply.github.com>
2018-02-21 10:28:46 -0500

    2/21/18
    jsCalc

---
+++ jsCalc.js
@@ -0,0 +1,20 @@
+function add(num1, num2){
+    return num1+num2;
+}
+function subtract(num1, num2){
+    return num1-num2;
+}
+function multiply(num1, num2){
+    return num1*num2;
+}
+function divide(num1, num2){
+    return num1/num2;
+}   
+
+
+var sum = add(4,2);
+var diff = subtract(4,2);
+var product = multiply(4,2);
+var dividend = divide(4,2);
+
+console.log("These are the Results: " + sum + "  " + diff + "  " + product + "  " + dividend)
