976352372bd98ff11eeb1df0c6cfb11b2672ebee
AngelinaBalliro <35516348+aeoniandesign@users.noreply.github.com>
2018-02-28 13:57:13 -0500

    App Light and JS Event
    App light updates, color, text size, floating in switch.
    JS Event walkthrough.

--- appLight/appLight.js
+++ appLight/appLight.js
@@ -10,10 +10,12 @@ function flipswitch() {
 		document.body.style.backgroundColor = "black";
 		document.getElementById("position").innerHTML = "OFF";
     document.getElementById("position").style.color ="black";
+		document.getElementById("position").style.cssFloat ="right";
 	}else{
 		light.style.cssFloat="right";
 		document.body.style.backgroundColor = "white";
 		document.getElementById("position").innerHTML = "ON";
     document.getElementById("position").style.color ="white";
+		document.getElementById("position").style.cssFloat ="left";
 	}
 }
