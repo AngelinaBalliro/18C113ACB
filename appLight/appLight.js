026f53e8715bc4879299ad9e95c3ac04674bc4b7
AngelinaBalliro <35516348+aeoniandesign@users.noreply.github.com>
2018-02-28 10:22:18 -0500

    App Light
    App Light

---
+++ appLight/appLight.js
@@ -0,0 +1,19 @@
+function flipswitch() {
+	var light = document.getElementById("switcher");
+	if(light.style.cssFloat == "") {
+		light.style.cssFloat = "left";
+		document.body.style.backgroundColor = "black";
+		document.getElementById("position").innerHTML = "OFF";
+	}
+	if(light.style.cssFloat == "right"){
+		light.style.cssFloat="left";
+		document.body.style.backgroundColor = "black";
+		document.getElementById("position").innerHTML = "OFF";
+    document.getElementById("position").style.color ="black";
+	}else{
+		light.style.cssFloat="right";
+		document.body.style.backgroundColor = "white";
+		document.getElementById("position").innerHTML = "ON";
+    document.getElementById("position").style.color ="white";
+	}
+}
