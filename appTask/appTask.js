beb06300d224ca08ecb20a0a61614c77b0edef9c
Angelina <angie.413@icloud.com>
2018-03-21 01:22:35 -0400

    App Task Correction
    Corrected issue that only the last new task will be deleted. Now the task at the end of the list will be deleted

--- appTask/appTask.js
+++ appTask/appTask.js
@@ -3,8 +3,9 @@ function taskManager() {
   $("#add").on("click", function() {
     let newTask = $("#taskInput").val();
     $newListItem = $("<li></li>").text(newTask);
+    let lastItem = $newListItem;
     $("#delete").on("click", function() {
-      $newListItem.remove();
+      lastItem.remove(":last-of-type");
     });
     $("#list").append($newListItem);
   });
